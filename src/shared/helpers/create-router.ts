106cc2307e6b4f364585444381e2fa06698f7769
ptts <patrick.steffen@filu.vet>
2025-01-14 00:35:07 +0100

    refactor: enable non-strict mode in router creation

--- src/shared/helpers/create-router.ts
+++ src/shared/helpers/create-router.ts
@@ -1,4 +1,4 @@
 import { Hono } from 'hono';
 import type { AppEnv } from '../../types';
 
-export const createRouter = () => new Hono<AppEnv>();
+export const createRouter = () => new Hono<AppEnv>({ strict: false });
