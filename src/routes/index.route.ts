008e3b092e74b3b72cb9494de80fb81a7948b3ea
ptts <patrick.steffen@filu.vet>
2025-01-13 16:33:40 +0100

    refactor: remove development route logging and simplify index route response

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -1,15 +1,8 @@
-import { app } from '../app';
 import { createRouter } from '../shared/helpers/create-router';
 
 const indexRouter = createRouter();
 
-import { showRoutes } from 'hono/dev';
-
 indexRouter.get('/', (c) => {
-  if (c.env.ENVIRONMENT === 'development') {
-    showRoutes(app);
-  }
-
   return c.json({
     message: 'Sonuby Backend',
     environment: c.env.ENVIRONMENT,
